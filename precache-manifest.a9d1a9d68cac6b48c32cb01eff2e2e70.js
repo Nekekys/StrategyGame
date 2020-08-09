62fd0b396e823d9f21c35ddeb82aebde5b68b2fd
Nekekys <45885162+Nekekys@users.noreply.github.com>
2020-08-09 17:42:44 +0300

    Add files via upload

---
+++ precache-manifest.a9d1a9d68cac6b48c32cb01eff2e2e70.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735e88fe0c6b20e26439a0b22599a89d",
+    "url": "/StrategyGame/index.html"
+  },
+  {
+    "revision": "1611406217f0c366f679",
+    "url": "/StrategyGame/static/css/main.0affea45.chunk.css"
+  },
+  {
+    "revision": "ac24b273b97e87df1b02",
+    "url": "/StrategyGame/static/js/2.577cea00.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/StrategyGame/static/js/2.577cea00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1611406217f0c366f679",
+    "url": "/StrategyGame/static/js/main.d7566181.chunk.js"
+  },
+  {
+    "revision": "6b9031de5d5a2c6b4430",
+    "url": "/StrategyGame/static/js/runtime-main.2ba03d02.js"
+  },
+  {
+    "revision": "5a41ef796517c317db7cae078d1e97ae",
+    "url": "/StrategyGame/static/media/Call_of_Duty_Infinite_Warfare_1_1.5a41ef79.png"
+  },
+  {
+    "revision": "374a036248d21c6dd8b495568981b469",
+    "url": "/StrategyGame/static/media/Call_of_Duty_Infinite_Warfare_3_2.374a0362.png"
+  },
+  {
+    "revision": "0fe5dbc84c613ef6efe74adef7e852b5",
+    "url": "/StrategyGame/static/media/Deus_Ex_Mankind_Divided_1_1.0fe5dbc8.png"
+  },
+  {
+    "revision": "6ae1900d39808333fd0a376d9bb6585a",
+    "url": "/StrategyGame/static/media/Deus_Ex_Mankind_Divided_3_2.6ae1900d.png"
+  },
+  {
+    "revision": "07bfc2dc6e1ffd824bc376dac37bec4f",
+    "url": "/StrategyGame/static/media/Hearts-of-Iron.07bfc2dc.jpg"
+  },
+  {
+    "revision": "84325a3b5a8c2b95144f3fbff1e3a5da",
+    "url": "/StrategyGame/static/media/Madden_NFL_17_1_1.84325a3b.png"
+  },
+  {
+    "revision": "77b717ae53ecce1e06028dc2739cf830",
+    "url": "/StrategyGame/static/media/Madden_NFL_17_3_2.77b717ae.png"
+  },
+  {
+    "revision": "b04a2b81f28234af1e78746f2fe2a209",
+    "url": "/StrategyGame/static/media/Mafia_3_1_1.b04a2b81.png"
+  },
+  {
+    "revision": "c675139d8dd01c42102da829c8b65ac1",
+    "url": "/StrategyGame/static/media/Mafia_3_3_2.c675139d.png"
+  },
+  {
+    "revision": "01eadf142e1f0e48cf0aafc225094dca",
+    "url": "/StrategyGame/static/media/Sherlock_Holmes_The_Devils_Daughter_1_1.01eadf14.png"
+  },
+  {
+    "revision": "1ac357f2dfb5363e8d36eeeda09a1dd2",
+    "url": "/StrategyGame/static/media/Sherlock_Holmes_The_Devils_Daughter_3_2.1ac357f2.png"
+  },
+  {
+    "revision": "54aa194c91d274fc557c793f8e2e76e4",
+    "url": "/StrategyGame/static/media/The_Technomancer_1_1.54aa194c.png"
+  },
+  {
+    "revision": "6302b792eef25afdc6e188a2aa1793c1",
+    "url": "/StrategyGame/static/media/The_Technomancer_3_2.6302b792.png"
+  },
+  {
+    "revision": "b369a94eaa260be82c091a5eaed0774d",
+    "url": "/StrategyGame/static/media/back.b369a94e.svg"
+  },
+  {
+    "revision": "f9c38fee8a4a55c4dba2ec8292d7c377",
+    "url": "/StrategyGame/static/media/batlefront_1_1.f9c38fee.png"
+  },
+  {
+    "revision": "2c622e3dd373633958349a937fd3e1af",
+    "url": "/StrategyGame/static/media/batlefront_3_2.2c622e3d.png"
+  },
+  {
+    "revision": "7d92356565ee94857e0f908417955162",
+    "url": "/StrategyGame/static/media/civilization6.7d923565.jpg"
+  },
+  {
+    "revision": "e9a644c930886e8b2f4001164455052e",
+    "url": "/StrategyGame/static/media/frostpunk.e9a644c9.jpg"
+  },
+  {
+    "revision": "aabceabcaee55649f2aabed7c50e00e1",
+    "url": "/StrategyGame/static/media/gtafans-ru-gta.aabceabc.jpg"
+  },
+  {
+    "revision": "6ef52c6afee64abb85ac4ec5e40e065d",
+    "url": "/StrategyGame/static/media/next.6ef52c6a.svg"
+  },
+  {
+    "revision": "945f8d0c6da3b103f60fd2f622240565",
+    "url": "/StrategyGame/static/media/shopping-cart.945f8d0c.svg"
+  },
+  {
+    "revision": "31c4b34a1b44d34fe2df4e28aea82201",
+    "url": "/StrategyGame/static/media/star-wars-battlefront-2.31c4b34a.jpg"
+  },
+  {
+    "revision": "e2dc6310f0256c84875507f17f1dc83b",
+    "url": "/StrategyGame/static/media/star.e2dc6310.svg"
+  },
+  {
+    "revision": "d308d00f5342f542fe1f4842dcf76e89",
+    "url": "/StrategyGame/static/media/star_2.d308d00f.svg"
+  },
+  {
+    "revision": "068d9b0653be6776f91db9f113bda354",
+    "url": "/StrategyGame/static/media/user.068d9b06.svg"
+  }
+]);
